43c74ca50fc7bf21e980f2ba50531145c5d48476
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-19 19:15:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function theBeatlesPlay(arrayPlayers, arrayInstruments) {
 function johnLennonFacts(arrayFacts) {
   // While Loop
   let counter = 0;
-  // Copy array
+  var newArray = Object.assign({}, arrayFacts)
   while(counter < arrayFacts.length){
     counter += 1;
     console.log(arrayFacts[counter] + " !!!");
