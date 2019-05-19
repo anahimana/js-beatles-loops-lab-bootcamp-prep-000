04a3d91172cd099574ee95c3c2febd3713cbfe31
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-19 19:13:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function johnLennonFacts(arrayFacts) {
   let counter = 0;
   while(counter < arrayFacts.length){
     counter += 1;
-    console.log(arrayFacts[counter] + "!!!");
+    console.log(arrayFacts[counter] + " !!!");
   }
 }
 
