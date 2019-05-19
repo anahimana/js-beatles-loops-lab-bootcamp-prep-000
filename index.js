1dd7aa5a30bca4af49c17e5df6d88e188535552d
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-19 19:06:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,3 +19,10 @@ function theBeatlesPlay(arrayPlayers, arrayInstruments) {
 function johnLennonFacts(arrayFacts) {
   // do something
 }
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
